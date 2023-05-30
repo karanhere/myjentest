910198460af63f2d352a310a37e4f7045abc752c
karan <kc95155@gmail.com>
2023-05-30 12:21:21 +0530

    added details to azuread provider block

--- provider.tf
+++ provider.tf
@@ -18,5 +18,8 @@ provider "azurerm" {
 
 provider "azuread" {
   # Configuration options
+    subscription_id = var.subscription_id
     tenant_id = var.tenant_id
+    client_id = var.client_id
+    client_secret = var.client_secret
 }
