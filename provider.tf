d825233f88affdd278907e1755306ce38dd5d053
karan <kc95155@gmail.com>
2023-05-30 12:26:51 +0530

    removed azuread provider

--- provider.tf
+++ provider.tf
@@ -16,10 +16,3 @@ provider "azurerm" {
     features {}
 }
 
-provider "azuread" {
-  # Configuration options
-    subscription_id = var.subscription_id
-    tenant_id = var.tenant_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-}
